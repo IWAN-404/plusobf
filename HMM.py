f70efa306d411c852d20e8e9ae23821efbe8d94d
Iwan Hadiansah ID <79639974+IWAN-404@users.noreply.github.com>
2021-03-16 11:09:22 +0700

    Add files via upload

---
+++ HMM.py
@@ -0,0 +1,46 @@
+print ANAK KONTOL MAU RECODE TINGGAL FAKE APA SUSAH
+print NYA BABI BANGSAT ASW KONTOL MEMEK ITIL BANGSAT
+# AUTHOR : IWAN HADIANSAH ID
+# FACEBOOK : https://www.facebook.com/Iwan.Devoleper
+# WHATSAPP : https://wa.me/085766833********************BACOT
+# DEVOLEPER : LAMPUNG SELATAN
+
+╭╮╮╱▔▔▔▔╲╭╭╮
+╰╲╲▏▂╲╱▂▕╱╱╯
+┈┈╲▏▇▏▕▇▕╱┈┈
+┈┈╱╲▔▕▍▔╱╲┈┈
+╭╱╱▕╋╋╋╋▏╲╲╮
+╰╯╯┈╲▂▂╱┈╰╰╯
+RECODE ANAK KONTOL
+www.kang recode lupa ngocok
+
+╭╮╮╱▔▔▔▔╲╭╭╮
+╰╲╲▏▂╲╱▂▕╱╱╯
+┈┈╲▏▇▏▕▇▕╱┈┈
+┈┈╱╲▔▕▍▔╱╲┈┈
+╭╱╱▕╋╋╋╋▏╲╲╮
+╰╯╯┈╲▂▂╱┈╰╰╯
+
+
+╭╮╮╱▔▔▔▔╲╭╭╮
+╰╲╲▏▂╲╱▂▕╱╱╯
+┈┈╲▏▇▏▕▇▕╱┈┈
+┈┈╱╲▔▕▍▔╱╲┈┈
+╭╱╱▕╋╋╋╋▏╲╲╮
+╰╯╯┈╲▂▂╱┈╰╰╯
+
+
+╭╮╮╱▔▔▔▔╲╭╭╮
+╰╲╲▏▂╲╱▂▕╱╱╯
+┈┈╲▏▇▏▕▇▕╱┈┈
+┈┈╱╲▔▕▍▔╱╲┈┈
+╭╱╱▕╋╋╋╋▏╲╲╮
+╰╯╯┈╲▂▂╱┈╰╰╯
+
+
+╭╮╮╱▔▔▔▔╲╭╭╮
+╰╲╲▏▂╲╱▂▕╱╱╯
+┈┈╲▏▇▏▕▇▕╱┈┈
+┈┈╱╲▔▕▍▔╱╲┈┈
+╭╱╱▕╋╋╋╋▏╲╲╮
+╰╯╯┈╲▂▂╱┈╰╰╯
